2e8f2b1cbc937a94ae9d5431f4db004119e62439
Liz Burton <liz.burton147@gmail.com>
2017-12-29 21:14:47 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(tardies) FROM students;"
+  "SELECT COUNT(tardies) FROM students WHERE tardies IS NOT NULL;"
 end
 
 def average_gpa_for_9th_grade
